29323b8701ba8bb8fb289e577654bbb71cd65cb3
frtzkrz <friedrichkaspargiuliano.kurz@uzh.ch>
2025-05-14 16:06:32 +0200

    correct saving

--- thesis_plots/scripts/prevalences.py
+++ thesis_plots/scripts/prevalences.py
@@ -15,12 +15,12 @@ import lyscripts.plot.histograms as lyhist
 
 # Specify models and respective data source locations
 MODELS = {}
-for tag in ["VI_VII_A_prevalences", "VI_VII_B_prevalences", "VI_VII_D_prevalences"]:
+for tag in ["I_A_prevalences", "I_B_prevalences", "I_C_prevalences"]:
     MODELS[tag] = Path(f"thesis_plots/graph_results/prevalences/{tag}.hdf5")
 
 modelnames = [name for name in MODELS.keys()]
 
-filename = 'VI_VII_2'
+filename = 'I_1'
 
 # define USZ colors
 COLORS = {
@@ -31,17 +31,18 @@ COLORS = {
     # "gray": '#c5d5db',
 }
 COLOR_CYCLE = cycle(COLORS.values())
-BINS = np.linspace(0., 20., 200)
+BINS = np.linspace(0., 14., 200)
 HIST_KWARGS = {
     "density": True,
     "bins": BINS,
     "histtype": "stepfilled",
     "alpha": 0.7,
 }
+['Ioverall','InotII', 'IInotI', 'IIoverall', 'IandII', ]
 
 SCENARIO_DICT = {
-    "VIoverall": "VI overall",
-    "VInotII": "VI w/o II",
+    "Ioverall": "I overall",
+    "InotII": "I w/o II",
 }
 scenario_names = [name for name in SCENARIO_DICT.keys()]
 
@@ -51,11 +52,11 @@ if __name__ == "__main__":
     fig, ax = plt.subplot_mosaic(
         [[f'{modelname}/early', f'{modelname}/late'] for modelname in MODELS.keys()],
         sharex=True, sharey=True,
-        figsize=lyhist.get_size(width="full"),
+        figsize=(6.299212598425196, 3.893209269731271*1.25),
     )
 
     for i, (modelname, filepath) in enumerate(MODELS.items()):
-        ylabel = modelname.split("_")[2]
+        ylabel = modelname.split("_")[1]
         with h5py.File(name=filepath, mode="r") as h5_file:
             for scenario in scenario_names:
                 color = next(COLOR_CYCLE)
@@ -84,7 +85,7 @@ if __name__ == "__main__":
                         color=color,
                     )
 
-                    ax[axname].set_ylim(0., 0.6)
+                    ax[axname].set_ylim(0., 0.90)
                     if stage == "early":
                         ax[axname].set_ylabel(ylabel)
 
